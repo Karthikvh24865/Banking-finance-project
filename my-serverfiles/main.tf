bdc0fb907ab8f4c8a8e5768ec19b0f987fcb5706
Karthikvh24865 <111963116+Karthikvh24865@users.noreply.github.com>
2025-04-20 22:03:55 +0530

    Update main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "test-server" {
   ami           = "ami-00bb6a80f01f03502" 
   instance_type = "t2.micro" 
-  key_name = "Prabhu"
-  vpc_security_group_ids= ["sg-0ce7a707b375e270d"]
+  key_name = "huli"
+  vpc_security_group_ids= ["sg-0f9b64f0f5bafa7e0"]
   connection {
     type     = "ssh"
     user     = "ubuntu"
-    private_key = file("./Prabhu.pem")
+    private_key = file("./huli.pem")
     host     = self.public_ip
   }
   provisioner "remote-exec" {
@@ -22,7 +22,7 @@ resource "aws_instance" "test-server" {
 //  command = "echo ${aws_instance.test-server.public_ip} | sudo tee /etc/ansible/hosts > /dev/null "
 //}
   provisioner "local-exec" {
-    command = "echo '${self.public_ip} ansible_ssh_user=ubuntu ansible_ssh_private_key_file=./Prabhu.pem' | sudo tee -a /etc/ansible/hosts > /dev/null"
+    command = "echo '${self.public_ip} ansible_ssh_user=ubuntu ansible_ssh_private_key_file=./huli.pem' | sudo tee -a /etc/ansible/hosts > /dev/null"
   }
 
    provisioner "local-exec" {

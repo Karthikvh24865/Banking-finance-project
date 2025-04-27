8ac349ff36b452e26b30839c9ccf13459b4d2fde
Karthikvh24865 <111963116+Karthikvh24865@users.noreply.github.com>
2025-04-27 08:30:44 +0530

    Update main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -9,9 +9,6 @@ resource "aws_instance" "test-server" {
     private_key = file("./huli.pem")
     host     = self.public_ip
   }
-  lifecycle {
-    prevent_destroy = true
-  }
   provisioner "remote-exec" {
     inline = [ "echo 'wait to start instance' "]
   }

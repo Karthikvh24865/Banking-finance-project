d87737c28e89be03501b72cbf4995d92a8e9cc94
Karthikvh24865 <111963116+Karthikvh24865@users.noreply.github.com>
2025-04-27 08:16:13 +0530

    Update main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "test-server" {
     Name = "test-server"
   }
  provisioner "local-exec" {
-      command = " echo ${aws_instance.test-server.public_ip} > /etc/ansible/hosts "
+      command = "sudo echo ${aws_instance.test-server.public_ip} > /etc/ansible/hosts "
 }
 //provisioner "local-exec" {
  // command = "echo ${aws_instance.test-server.public_ip} > /etc/ansible/hosts "
